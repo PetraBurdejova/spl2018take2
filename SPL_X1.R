6b275cb2041d2b99a6d805f35262d47840799c03
warsihas <haseebw15@gmail.com>
2018-05-29 22:42:57 +0200

    got get.data function to work, it's on line 41 - 51, Just add whateve characteristics you want to vector y and they will get added to the aggregated dataset

--- SPL_X1.R
+++ SPL_X1.R
@@ -23,8 +23,7 @@ str(df1)
 ###Create a dataframe of codes for each neighbourhood
 neigh.codes <- as.data.frame(cbind(colnames(df1[,-c(1,2)]), as.vector(unlist(b[1,-c(1:4)]))))
 colnames(neigh.codes) <- c("Neighborhood", "Hood_ID")
-j <- as.data.frame(cbind(unlist(b[b$Characteristic == "Population, 2016",-c(1:4)]), neigh.codes$Hood_ID))
-colnames(j) <- c("Population, 2016", "Hood_ID")
+
 
 #####Remove thousands seperator commas from numbers and replace % sign eith e-2, 
 ####so we can use as.numeric to convert from a character to a number
@@ -36,6 +35,22 @@ df1[df1 == "n/a"] <- NA
 
 ####Turn columns into numeric
 df1[,-c(1,2)] <- lapply(df1[,-c(1,2)], as.numeric)
+
+###TESTING
+
+agg <- read.csv("aggregated.csv")
+get.data <- function(x, z, y) {
+  m <- as.data.frame(cbind(unlist(x[x$Characteristic == y, -c(1:2)]), neigh.codes$Hood_ID))
+  colnames(m) <- c(y, "Hood_ID")
+  p <- merge(m,z, by.x = "Hood_ID", by.y = "Hood_ID")
+  return(p)
+}
+y <- c("Population, 2016", "Population, 2011", "Male: 0 to 04 years", "Male: 05 to 09 years", "Total private dwellings")
+for(i in y){
+  agg <- get.data(df1, agg, i)
+}
+
+##END TEST
 
 ###Turn data from wide to long format using melt
 df2 <- melt(df1, id.vars=c("Topic", "Characteristic"))
