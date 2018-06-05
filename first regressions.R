a2e5a2ae9596c27d274b26ad23b3707c46a95de3
warsihas <haseebw15@gmail.com>
2018-06-05 17:07:11 +0200

    need to filter out old occurence dates

--- first regressions.R
+++ first regressions.R
@@ -1,21 +1,46 @@
+library(data.table)
+library(reshape2)
+library(forcats)
+library(dplyr)
 #####create agg#####
 
 #read csvs
 a <- read.csv("MCI_2014_to_2017.csv")
 b <- read.csv("2016_neighbourhood_profiles.csv")
-agg <- read.csv("aggregated.csv")
+
+#Create aggregate data frame using data table
+a.dt <- as.data.table(a)
+setkey(a.dt, "MCI", "Hood_ID")
+agg <- a.dt[, .(count = .N), by = c("MCI", "Hood_ID")]
+agg <- dcast(agg, Hood_ID ~ MCI)
+agg$Hood_ID <- as.factor(agg$Hood_ID)
 
 #b --> df1
 df1 <- as.data.frame(b)
-df1 <- b[-c(1,2),-c(1,3)]
-neigh.codes <- as.data.frame(cbind(colnames(df1[,-c(1,2)]), as.vector(unlist(b[1,-c(1:4)]))))
+df1 <- b[-c(1,2),-c(1,2,3,5)]
+
+###Data frame with Hood IDs
+neigh.codes <- as.data.frame(cbind(colnames(df1[,-1]), as.vector(unlist(b[1,-c(1:5)]))))
 colnames(neigh.codes) <- c("Neighborhood", "Hood_ID")
-df1[,-c(1,2)] <- lapply(df1[,-c(1,2)], function(x) {gsub(",", "", x)})
-df1[,-c(1,2)] <- lapply(df1[,-c(1,2)], function(x) {gsub("%", "e-2", x)})
+
+###remove strange characters in dataframe 
+df1[,-1] <- lapply(df1[,-1], function(x) {gsub(",", "", x)})
+df1[,-1] <- lapply(df1[,-1], function(x) {gsub("%", "e-2", x)})
 df1[df1 == "n/a"] <- NA
-df1[,-c(1,2)] <- lapply(df1[,-c(1,2)], as.numeric)
+df1[,-1] <- lapply(df1[,-1], as.numeric)
+str(df1)
+
+####HERE
+df1 <- fct_collapse(df1$Characteristic,
+                    "young_male" = c("Male: 15 to 19 years", "Male: 20 to 24 years", "Male: 25 to 29 years",
+                                   "Male: 30 to 34 years", "Male: 35 to 39 years"))
+
+
+
+
 get.data <- function(x, z, y) {
-  m <- as.data.frame(cbind(unlist(x[x$Characteristic == y, -c(1:2)]), neigh.codes$Hood_ID))
+  m <- as.data.frame(cbind(unlist(x[x$Characteristic == y, -c(1:2)]), neigh.codes))
+  m$Neighborhood <- NULL
   colnames(m) <- c(y, "Hood_ID")
   p <- merge(m,z, by.x = "Hood_ID", by.y = "Hood_ID")
   return(p)
@@ -26,6 +51,7 @@ y <- c("Population, 2016", "Unemployed")
 for(i in y){
   agg <- get.data(df1, agg, i)
 }
+
 
 #adjust for population size
 agg$Assault <- agg$Assault/agg$`Population, 2016`
