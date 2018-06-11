5c7cf18f8eec2d4738f3c520a7e7379d7002518e
gablum <32772987+gablum@users.noreply.github.com>
2018-06-11 18:39:57 +0200

    Add files via upload
    code for regression

---
+++ regressioncode.R
@@ -0,0 +1,17 @@
+regressiondata <- read.csv("regressiondata.csv")
+
+model<-lm(Robbery~Unemployed,data=agg)
+summary(model)
+
+#plot errors
+plot(x=agg$Hood_ID,y=residuals(model),xlab="Hood", ylab="Residuals",panel.last = abline(h=0, lty=2))
+
+#autocorrelation
+library(car)
+dwt(model)
+
+#normailty
+library(nortest)
+ad.test(residuals(model)) 
+shapiro.test(residuals(model)) 
+lillie.test(residuals(model))
