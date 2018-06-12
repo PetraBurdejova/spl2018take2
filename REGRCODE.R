8f6f87170a3261e0426e598b65dd134bca909ebd
gablum <32772987+gablum@users.noreply.github.com>
2018-06-12 15:28:38 +0200

    regrcode

---
+++ REGRCODE.R
@@ -0,0 +1,45 @@
+#read csv
+regressiondata <- read.csv("regressiondata.csv")
+r <- regressiondata
+
+#dependent variables (dont run it)
+r$Assault
+r$Auto.Theft
+r$Break.and.Enter
+r$Robbery
+r$Theft.Over
+
+#independent variables (dont run it)
+r$lone.parent.families.perc
+r$avg.income
+r$low.income.pop.perc
+r$non.citizens.perc
+r$immigrants.recent.perc
+r$refugees.perc
+r$vis.minorities.perc
+r$renters.perc
+r$houses.perc
+r$less.than.high.school.perc
+r$high.school.cert.perc
+r$post.sec.or.above.perc
+r$unemployment.rate
+r$youth.perc
+
+
+#regression
+model<-lm(Assault~lone.parent.families.perc+avg.income+low.income.pop.perc+non.citizens.perc+immigrants.recent.perc+refugees.perc+vis.minorities.perc+
+            renters.perc+houses.perc+less.than.high.school.perc+high.school.cert.perc+post.sec.or.above.perc+unemployment.rate+youth.perc, data=r)
+summary(model)
+
+#plot errors
+plot(x=agg$Hood_ID,y=residuals(model),xlab="Hood", ylab="Residuals",panel.last = abline(h=0, lty=2))
+
+#autocorrelation
+library(car)
+dwt(model)
+
+#normailty
+library(nortest)
+ad.test(residuals(model)) 
+shapiro.test(residuals(model)) 
+lillie.test(residuals(model))
