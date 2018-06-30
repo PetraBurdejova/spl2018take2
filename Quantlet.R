5a5a12c124132919ea794e0e26957def2213bba6
degenhaf <degenhaf@cms.hu-berlin.de>
2018-06-30 11:39:41 +0200

    quantlet

---
+++ Quantlet.R
@@ -0,0 +1,20 @@
+devtools::install_github("lborke/yamldebugger")
+
+library(yamldebugger)
+
+allKeywords
+"plot" %in% allKeywords
+
+
+help(yaml.debugger.init)
+d_init = yaml.debugger.init("test", show_keywords = TRUE)
+
+
+help(yaml.debugger.get.qnames)
+qnames = yaml.debugger.get.qnames(d_init$RootPath)
+
+
+
+d_results = yaml.debugger.run(qnames, d_init)
+
+OverView = yaml.debugger.summary(qnames, d_results, summaryType = "mini")
