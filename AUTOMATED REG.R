cea0e07cd9866494c5cf527478d8a37d72f978f1
gablum <32772987+gablum@users.noreply.github.com>
2018-07-11 16:09:44 +0200

    automated regressions

--- AUTOMATED REG.R
+++ AUTOMATED REG.R
@@ -9,17 +9,8 @@ swD <- function (p, x) {
 }
 #firstregressionresults <- list()
 regressionresults <- list()
-means <- c()
-bptests <- c()
-swtests <- c()
-vif1 <- c()
-vif2 <- c()
-vif3 <- c()
-vif4 <- c()
-cortest1 <- c()
-cortest2 <- c()
-cortest3 <- c()
-cortest4 <- c()
+ols.ass <- as.data.frame(matrix(nrow=7, ncol=11))
+colnames(ols.ass) <- c("means", "bptests", "swtests", "vif1", "vif2", "vif3", "vif4", "cortest1", "cortest2", "cortest3", "cortest4")
 
 firstdensity <- list()
 firsthistogram <- list()
@@ -38,31 +29,30 @@ for (i in 2:8){
   model <- lm(tmp.bp~male.youth+less.than.high.school+low.income+immigrants, data=rtmp)
   regressionresults[[i-1]]<-summary(model)
   #Assumption: error term has a population mean of zero - irrelevant as we include an intercept
-  means[i-1] <- mean(model$residuals)
+  ols.ass$means[i-1] <- mean(model$residuals)
   #Assumption: no serial correlation of the error term 
   #irrelevant as observation order is random
   #Assumption: the error term is homoscedastic
-  bptests[i-1] <- bptest(model)$p.value #sensitive to asumption of normality
+  ols.ass$bptests[i-1] <- bptest(model)$p.value #sensitive to asumption of normality
   #Assumption: error term is normally distributed (optional: not required for OLS, but allows to performa statistical hypothesis testing and generate reliable confidence intervals)
-  swtests[i-1] <- shapiro.test(residuals(model))$p.value
+  ols.ass$swtests[i-1] <- shapiro.test(residuals(model))$p.value
   #Assumption: regression model is linear in the coefficients and the error term
 #     ceresPlots(model) #crPlots(model.assault)
   #Assumption: no (perfect) multicollinearity within the independent variables - variance inflation factors
-  vif1[i-1] <- vif(model)[1]
-  vif2[i-1] <- vif(model)[2]
-  vif3[i-1] <- vif(model)[3]
-  vif4[i-1] <- vif(model)[4]
+  ols.ass$vif1[i-1] <- vif(model)[1]
+  ols.ass$vif2[i-1] <- vif(model)[2]
+  ols.ass$vif3[i-1] <- vif(model)[3]
+  ols.ass$vif4[i-1] <- vif(model)[4]
 #     corrplot::corrplot(cor(r_assault[c(),]))
   #Assumption: no correlation of each independent variable with the error term
-  cortest1[i-1] <- cor.test(rtmp$male.youth, model$residuals)$p.value
-  cortest2[i-1] <- cor.test(rtmp$less.than.high.school, model$residuals)$p.value
-  cortest3[i-1] <- cor.test(rtmp$low.income, model$residuals)$p.value
-  cortest4[i-1] <- cor.test(rtmp$immigrants, model$residuals)$p.value
+  ols.ass$cortest1[i-1] <- cor.test(rtmp$male.youth, model$residuals)$p.value
+  ols.ass$cortest2[i-1] <- cor.test(rtmp$less.than.high.school, model$residuals)$p.value
+  ols.ass$cortest3[i-1] <- cor.test(rtmp$low.income, model$residuals)$p.value
+  ols.ass$cortest4[i-1] <- cor.test(rtmp$immigrants, model$residuals)$p.value
   firstdensity[[i-1]] <- density(r$tmp)
   firsthistogram[[i-1]] <- hist(r$tmp)
   density[[i-1]] <- density(rtmp$tmp.bp)
-  histogram[[i-1]] <- hist(rtmp$tmp.bp) 
-}
-rm(rtmp, exponent, i, outliers, model, swD)
-ols.assumptions <- data.frame(means, bptests, swtests, cortest1, cortest2, cortest3, cortest4, vif1, vif2, vif3, vif4)
-rm(means, bptests, swtests, vif1, vif2, vif3, vif4, cortest1, cortest2, cortest3, cortest4)
+  histogram[[i-1]] <- hist(rtmp$tmp.bp)
+  rm(exponent, model, outliers, rtmp)
+  }
+rm(i, swD)
