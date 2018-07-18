5f6386e7bdac20fbf9ea3f42b6ad0108beba1351
warsihas <haseebw15@gmail.com>
2018-07-18 17:01:54 +0200

    repeat

--- F-Test.R
+++ F-Test.R
@@ -1,4 +1,21 @@
 
+reg1 <- lm(assault ~ youth, data = agg.2016)
+
+
+summary(reg1)
+
+
+RSS0 <- sum((agg.2016$assault - mean(agg.2016$assault))^2) #20181.97, this is same as TSS really
+
+RSS <- sum(reg1$residuals^2) #20181.64
+
+p <-  1 #predictors whos coefficient we are testing.
+
+n <- length(agg.2016$youth) #number of observations
+
+F <- ( (RSS0-RSS)/p ) / (RSS/(n-p-1))
+
+F
 
 
 reg2 <- lm(total.crime ~ youth + immigrants, data = agg.2016)
