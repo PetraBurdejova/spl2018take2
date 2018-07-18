1c04b6007619cbeda30e67882be80c14b0df8d00
degenhaf <degenhaf@cms.hu-berlin.de>
2018-07-18 15:12:49 +0200

    q

--- Install_Packages.R
+++ Install_Packages.R
@@ -1,36 +1,65 @@
+# 
+# 
+# 
+# if(!require("pacman")) install.packages("pacman", dependencies = TRUE); library("pacman")
+# 
+# 
+# 
+# pacman::p_load("tidyverse", 
+#                "xts", 
+#                "zoo", 
+#                "chron", 
+#                "reshape2", 
+#                "ggplot2", 
+#                "ggmap", 
+#                "viridis", 
+#                "forecast",
+#                "sp",
+#                "rgdal",
+#                "data.table",
+#                "maptools",
+#                "sf",
+#                "lwgeom", 
+#                "nabor",
+#                "plyr",
+#                "spdep",
+#                "cluster",
+#              "scales",
+#                "caret",
+#              "scales", 
+#                "readr",
+#                "stingr")
 
+packages <- c("tidyverse", 
+              "xts", 
+              "zoo", 
+              "chron", 
+              "reshape2", 
+              "ggplot2", 
+              "ggmap", 
+              "viridis", 
+              "forecast",
+              "sp",
+              "rgdal",
+              "data.table",
+              "maptools",
+              "sf",
+              "lwgeom", 
+              "nabor",
+              "plyr",
+              "spdep",
+              "cluster",
+              "scales",
+              "caret",
+              "readr",
+              "stringr")
 
+usePackage <-function(p){
+  if (!is.element(p, installed.packages()[,1])){
+    print(paste('Package:',p,'Not found, Installing Now...'))
+    install.packages(p, dep = TRUE)}
+  print(paste('Loading Package :',p))
+  require(p, character.only = TRUE)  
+}
 
-if(!require("pacman")) install.packages("pacman", dependencies = TRUE); library("pacman")
-
-
-
-pacman::p_load("tidyverse", 
-               "xts", 
-               "zoo", 
-               "chron", 
-               "reshape2", 
-               "ggplot2", 
-               "ggmap", 
-               "viridis", 
-               "forecast",
-               "sp",
-               "rgdal",
-               "data.table",
-               "maptools",
-               "sf",
-               "lwgeom", 
-               "nabor",
-               "plyr",
-               "spdep",
-               "cluster",
-             "scales",
-               "caret",
-             "scales", 
-               "readr",
-               "stingr")
-
-if(!require("tidyverse")) install.packages("tidyverse", dependencies = TRUE); library("tidyverse")
-if(!require("reshape2")) install.packages("reshape2", dependencies = TRUE); library("reshape2")
-if(!require("ggmap")) install.packages("ggmap", dependencies = TRUE); library("ggmap")
-if(!require("stingr")) install.packages("stingr", dependencies = TRUE); library("stingr")
+for(i in packages) {(usePackage(i))}
