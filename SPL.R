9a6b468c418543878fd6f6f63745919696457100
warsihas <haseebw15@gmail.com>
2018-08-12 03:06:19 +0200

    changeddirectory

--- SPL.R
+++ SPL.R
@@ -2,12 +2,12 @@ rm(list=ls())
 
 # set working directory
 
-  setwd("C:/Users/Felix/Desktop/Uni/Berlin/2. Semester/SPL/spl2018take2")
+  setwd("../spl2018take2")
 
 
 # install all packages if needed
 
-  source("Install_packages.R")
+  source("../All_scripts/Install_packages/Install_Packages.R")
 
 # adding and manipulating datasets
   
